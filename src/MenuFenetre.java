8fd6362ac0479cc66a98d3bb7cc2c2e5abd97896
marionbriot <marion.briot@hotmail.fr>
2015-02-20 13:02:18 -0500

    partie de Guillaume

--- src/MenuFenetre.java
+++ src/MenuFenetre.java
@@ -10,15 +10,6 @@ Historique des modifications
 2013-05-03 Version initiale
  *******************************************************/  
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.KeyStroke;
-
 /******************************************************
 Cours:  LOG121
 Projet: Squelette du laboratoire #1
@@ -33,6 +24,7 @@ Historique des modifications
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -61,20 +53,20 @@ public class MenuFenetre extends JMenuBar{
 	MENU_AIDE_TITRE = "app.frame.menus.help.title",
 	MENU_AIDE_PROPOS = "app.frame.menus.help.about",
 	MENU_TRIER_TITRE = "app.frame.menus.sortBy.title",
-	MENU_TRIER_VALEURS_PAR_ID_CROISSANTE = "app.frame.menus.sortBy.ID";
-//	MENU_TRIER_VALEURS_PAR_ID_DECROISSANT = "app.frame.menus.sortBy.InverseID";
-//	MENU_TRIER_VALEURS_PAR_AIRE_CROISSANT = "app.frame.menus.sortBy.area";
-//	MENU_TRIER_VALEURS_PAR_AIRE_DECROISSANT = "app.frame.menus.sortBy.inverseArea";
-//	MENU_TRIER_VALEURS_PAR_FORMES = "app.frame.menus.sortBy.TypeOfShapes";
+	MENU_TRIER_VALEURS_PAR_ID_CROISSANTE = "app.frame.menus.sortBy.ID",
+	MENU_TRIER_VALEURS_PAR_ID_DECROISSANT = "app.frame.menus.sortBy.InverseID",
+	MENU_TRIER_VALEURS_PAR_AIRE_CROISSANT = "app.frame.menus.sortBy.area",
+	MENU_TRIER_VALEURS_PAR_AIRE_DECROISSANT = "app.frame.menus.sortBy.inverseArea",
+	MENU_TRIER_VALEURS_PAR_FORMES = "app.frame.menus.sortBy.TypeOfShapes",
 	//Le triage suivant se fait de la facon suivante : carrÈ -> rectangle -> cercle -> ovale -> ligne
-//	MENU_TRIER_VALEURS_PAR_FORMES_INVERSE = "app.frame.menus.sortBy.inverseTypeOfShapes";
+	MENU_TRIER_VALEURS_PAR_FORMES_INVERSE = "app.frame.menus.sortBy.inverseTypeOfShapes",
 	//le triage suivant se fait de la facon suivante : ligne -> ovale -> cercle -> rectangle -> carrÈ
-//	MENU_TRIER_VALEURS_PAR_DISTANCE_MAXIMAL = "app.frame.menus.sortBy.MaxDistance";
-//	MENU_TRIER_VALEURS_PAR_LARGEUR_CROISSANTE = "app.frame.menus.sortBy.Width";
-//	MENU_TRIER_VALEURS_PAR_LARGEUR_DECROISSANT = "app.frame.menus.sortBy.inverseWidth";
-//	MENU_TRIER_VALEURS_PAR_HAUTEUR_CROISSANT = "app.frame.menus.sortBy.Height";
-//	MENU_TRIER_VALEURS_PAR_HAUTEUR_DECROISSANTE = "app.frame.menus.sortBy.inverseHeight";
-//	MENU_TRIER_VALEURS_PAR_ORDRE_ORIGINAL = "app.frame.menus.sortBy.OriginalOrder";
+	MENU_TRIER_VALEURS_PAR_DISTANCE_MAXIMAL = "app.frame.menus.sortBy.MaxDistance",
+	MENU_TRIER_VALEURS_PAR_LARGEUR_CROISSANTE = "app.frame.menus.sortBy.Width",
+	MENU_TRIER_VALEURS_PAR_LARGEUR_DECROISSANT = "app.frame.menus.sortBy.inverseWidth",
+	MENU_TRIER_VALEURS_PAR_HAUTEUR_CROISSANT = "app.frame.menus.sortBy.Height",
+	MENU_TRIER_VALEURS_PAR_HAUTEUR_DECROISSANTE = "app.frame.menus.sortBy.inverseHeight",
+	MENU_TRIER_VALEURS_PAR_ORDRE_ORIGINAL = "app.frame.menus.sortBy.OriginalOrder";
 	//menu_trier_
 	private static final String MESSAGE_DIALOGUE_A_PROPOS = "app.frame.dialog.about";  
 	private JMenuItem arreterMenuItem, demarrerMenuItem;
@@ -91,17 +83,18 @@ public class MenuFenetre extends JMenuBar{
 	private JRadioButtonMenuItem sortByHauteurCroissante;
 	private JRadioButtonMenuItem sortByHauteurDecroissante;
 	private JRadioButtonMenuItem sortByOrdreServeur;
+	private static ButtonGroup groupJRadioButtonMenuItem = new ButtonGroup();
+	
 	CommBase comm; // Pour activer/d√©sactiver la communication avec le serveur
 	/**
 	 * Constructeur
 	 */
 	public MenuFenetre(CommBase comm) {
 		this.comm = comm;
-		addMenuTrierOptions();
 		addMenuDessiner();
 		addMenuFichier();
+		addMenuTrierOptions();
 		addMenuAide();
-		System.out.println(sortBySequenceCroissante.toString());
 	}
 	/**
 	 *  Cr√©er le menu "Draw". 
@@ -168,34 +161,84 @@ public class MenuFenetre extends JMenuBar{
 	 *  Cr√©er le menu "Trier". 
 	 */
 	private void addMenuTrierOptions() {
-		sortBySequenceCroissante = new JRadioButtonMenuItem("MENU_TRIER_TITRE");
-		System.out.println(sortBySequenceCroissante.toString());
-		JMenu menu = creerMenu (MENU_TRIER_TITRE, new JRadioButtonMenuItem[] { sortBySequenceCroissante });
+		sortBySequenceCroissante = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_ID_CROISSANTE);
+		sortBySequenceDecroissante = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_ID_DECROISSANT);
+		sortByAireFormeCroissante = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_AIRE_CROISSANT);
+		sortByAireFormeDecroissante = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_AIRE_DECROISSANT);
+		sortByTypeFormes = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_FORMES);
+		sortByTypeFormesInverse = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_FORMES_INVERSE);
+		sortByDistanceMaximale = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_DISTANCE_MAXIMAL);
+		sortByLargeurCroissante = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_LARGEUR_CROISSANTE);
+		sortByLargeurDecroissante = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_LARGEUR_DECROISSANT);
+		sortByHauteurCroissante = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_HAUTEUR_CROISSANT);
+		sortByHauteurDecroissante = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_HAUTEUR_DECROISSANTE);
+		sortByOrdreServeur = new JRadioButtonMenuItem(MENU_TRIER_VALEURS_PAR_ORDRE_ORIGINAL);
+				
+		
+		JMenu menu = creerMenu (MENU_TRIER_TITRE, new JRadioButtonMenuItem[] { sortBySequenceCroissante, sortBySequenceDecroissante, sortByAireFormeCroissante,sortByAireFormeDecroissante,sortByTypeFormes,sortByTypeFormesInverse, sortByDistanceMaximale,sortByLargeurCroissante, sortByLargeurDecroissante,  sortByHauteurCroissante, sortByHauteurDecroissante, sortByOrdreServeur });
 		menu.getItem(0).addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			System.out.println("Test 1er objet");
+			
 			}
 		});
-//		menu.getItem(1).addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//			System.out.println("Test 2e objet");
-//			}
-//		});
-//		menu.getItem(2).addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//			System.out.println("Test 3e objet");
-//			}
-//		});
-//		menu.getItem(3).addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//			System.out.println("Test 4e objet");
-//			}
-//		});
-//		menu.getItem(5).addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//			System.out.println("Test 5e objet");
-//			}
-//		});
+		menu.getItem(1).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 2e objet");
+			groupJRadioButtonMenuItem.clearSelection();
+			}
+		});
+    	menu.getItem(2).addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 3e objet");
+			groupJRadioButtonMenuItem.clearSelection();
+			}
+		});
+		menu.getItem(3).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 4e objet");
+			}
+		});
+		menu.getItem(4).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 5e objet");
+			}
+		});
+		menu.getItem(5).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 6e objet");
+			}
+		});
+		menu.getItem(6).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 7e objet");
+			}
+		});
+		menu.getItem(7).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 8e objet");
+			}
+		});
+		menu.getItem(8).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 9e objet");
+			}
+		});
+		menu.getItem(9).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 10e objet");
+			}
+		});
+		menu.getItem(10).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 11e objet");
+			}
+		});
+		menu.getItem(11).addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Test 12e objet");
+			}
+		});
 		KeyStroke.getKeyStroke(MENU_TRIER_VALEURS_TOUCHE_MASK,
 				MENU_TRIER_VALEURS_TOUCHE_RACC);
 		add(menu);
@@ -225,9 +268,10 @@ public class MenuFenetre extends JMenuBar{
 	private static JMenu creerMenu(String titleKey,JRadioButtonMenuItem[] itemKeys) {
 		JMenu menu = new JMenu(LangueConfig.getResource(titleKey));
 		for(int i=0; i < itemKeys.length; ++i) {
-			String itemKeysToString = "app.frame.menus.sortBy.title";
-			menu.add(new JMenuItem(LangueConfig.getResource(itemKeysToString)));
+			menu.add(new JRadioButtonMenuItem(LangueConfig.getResource(itemKeys[i].getText())));
+			groupJRadioButtonMenuItem.add(itemKeys[i]);
 		}
 		return menu;
 	}
+	
 }
